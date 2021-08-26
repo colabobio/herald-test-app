4c5118c1d2213a5d1e03ae36978c0323d5f763f0
codeanticode <andres.colubri@gmail.com>
2021-08-26 12:51:51 -0400

    process is working on iOS

--- HeraldTest-iOS/HeraldTest/TestService.swift
+++ HeraldTest-iOS/HeraldTest/TestService.swift
@@ -8,16 +8,30 @@
 import Foundation
 
 class TestService {
-    let timeStep: Int = 2
+    static let TIME_STEP: Int = 2
+    
+    private static var instance: TestService?
     
     var timer: Timer?
     var dispatch: DispatchSourceTimer?
+    
+    static var shared: TestService {
+        if let service = TestService.instance { return service }
+        let service = TestService()
+        TestService.instance = service
+        return service
+    }
+    
+    init() {
+        initState()
+        startTimer()
+    }
     
     private func startTimer() {
         let queue = DispatchQueue(label: "com.example.HeraldTest.timer", attributes: .concurrent)
         dispatch?.cancel() // cancel previous timer if any
         dispatch = DispatchSource.makeTimerSource(queue: queue)
-        dispatch?.schedule(deadline: .now(), repeating: .seconds(timeStep), leeway: .seconds(1))
+        dispatch?.schedule(deadline: .now(), repeating: .seconds(TestService.TIME_STEP), leeway: .seconds(1))
         dispatch?.setEventHandler { [weak self] in // `[weak self]` only needed if you reference `self` in this closure and you want to prevent strong reference cycle
             self?.update()
         }
@@ -31,6 +45,11 @@ class TestService {
         timer = nil
     }
     
+    private func initState() {
+        
+    }
+    
     private func update() {
+        print("update")
     }
 }
