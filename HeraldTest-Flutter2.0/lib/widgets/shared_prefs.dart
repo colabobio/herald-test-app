e4512c4f743be9800faa1f6f1ac6079be8f5575f
Andi Grozdani <38369370+agrozdani@users.noreply.github.com>
2021-12-15 22:22:14 -0500

    implemented storing identifier

---
+++ HeraldTest-Flutter2.0/lib/widgets/shared_prefs.dart
@@ -0,0 +1,23 @@
+import 'package:nanoid/nanoid.dart';
+import 'package:herald_flutter/constants/strings.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPrefs {
+  static late SharedPreferences _sharedPrefs;
+
+  factory SharedPrefs() => SharedPrefs._internal();
+
+  SharedPrefs._internal();
+
+  Future<void> init() async {
+    _sharedPrefs = await SharedPreferences.getInstance();
+  }
+
+  int get getIdentifier =>
+      _sharedPrefs.getInt(identifier) ??
+      int.parse(customAlphabet('1234567890', 9));
+
+  setIdentifier(int value) async {
+    await _sharedPrefs.setInt(identifier, value);
+  }
+}
